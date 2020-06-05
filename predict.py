5c7380f02cdb7b2b6053d1b363d832771c8d2b67
BOUHAFSADMI <sadmibouhafs@gmail.com>
2020-06-05 01:24:22 +0100

    Implementation of Prediction

---
+++ predict.py
@@ -0,0 +1,94 @@
+from utils import predict_build_argparser, process_image
+
+import numpy as np
+import pandas as pd
+import torch
+
+from torchvision import datasets, transforms, models
+
+from torch import nn, optim
+import torch.nn.functional as F
+
+import time
+import math
+import json
+
+
+
+def load_model(model_path):
+    
+    if "vgg16" in model_path:
+        model = models.vgg16(pretrained=True)
+    elif "vgg19" in model_path:
+        model = models.vgg19(pretrained=True)
+    elif "densenet121" in model_path:
+        model = models.densenet121(pretrained=True)        
+    elif "densenet161" in model_path:
+        model = models.densenet161(pretrained=True)        
+            
+    state = torch.load(model_path)
+    
+    model.classifier = state['classifier']
+    optim = state['optimizer']
+    
+    model.load_state_dict(state['state_dict'])
+    optim.load_state_dict(state['optimizer_state_dict'])
+    model.class_to_idx = state['class_to_idx']
+    
+    return model, optim
+
+
+def predict(image, model, cat_to_name, gpu=False, topk=5):
+    ''' Predict the class (or classes) of an image using a trained deep learning model.
+    '''
+    # predict the class from an image file
+    
+    image = image.unsqueeze(0)
+    
+    model.eval()
+    
+    if gpu:
+        model.gpu()
+    else:    
+        model.cpu()
+        
+    with torch.no_grad():
+        log_output = model.forward(image)
+        
+        output = torch.exp(log_output)
+        
+        top_prob, top_class = torch.topk(output, topk)
+        
+        labels_mapping_inv = {model.class_to_idx[i]: i for i in model.class_to_idx}
+        classes_labels = list()
+        for _class in top_class.numpy()[0]:
+            classes_labels.append(cat_to_name[labels_mapping_inv[_class]])
+    
+    return top_prob.numpy()[0], classes_labels
+    
+
+def main(args):
+    
+    print("\nStarting..!\n")
+    
+    image = process_image(args.input)
+    
+    model, optimizer = load_model(args.checkpoint) 
+    
+    with open(args.category_names, 'r') as f:
+        cat_to_name = json.load(f)
+    
+    probs, classes = predict(image, model, cat_to_name, args.gpu, args.top_k)
+    
+    print("Classes: ", classes)
+    print("Probabs: ", probs)
+
+    print("\nThe End..!\n")
+    
+ 
+if __name__ == "__main__":
+    
+    args = predict_build_argparser().parse_args()
+    
+    main(args)
+    
---
+++ utils.py
@@ -0,0 +1,68 @@
+from argparse import ArgumentParser
+from PIL import Image
+from torchvision import transforms
+
+
+def train_build_argparser():
+
+    parser = ArgumentParser()
+
+    parser.add_argument("-d", "--data_dir", required=True, type=str, default='flowers', help="Path to the directory that contains the data.")
+
+    parser.add_argument("-s", "--save_dir", required=False, type=str, default='./', help="Path to the directory to save the model.")
+
+    parser.add_argument("-a", "--arch", required=False, type=str, default='vgg16', help="Name of the Deep Learning model to use.")
+
+    parser.add_argument("-l", "--learning_rate", required=False, type=float, default=0.001, help="Learning rate hyperparameter.")
+
+    parser.add_argument("-u", "--hidden_units", required=False, type=int, default=4096, help="Hidden units hyperparameter.")
+
+    parser.add_argument("-e", "--epochs", required=False, type=int, default=20, help="Number of epochs.")
+    
+    parser.add_argument("-t", "--top_k", required=False, type=int, default=5, help="Number classes to display.")
+
+    parser.add_argument("-c", "--category_names", required=False, type=str, default='cat_to_name.json', help="Mapping of categories with real names.")
+    
+    parser.add_argument("-g", "--gpu", required=False, default=True, action='store_true', help="Chose to use gpu or not.")
+
+    return parser
+
+
+def predict_build_argparser():
+    
+     parser = ArgumentParser()
+        
+     parser.add_argument("-i", "--input", required=True, type=str, help="Image path to feed into the model.")
+    
+     parser.add_argument("-p", "--checkpoint", required=True, type=str, help="Path the saved model.")
+     
+     parser.add_argument("-t", "--top_k", required=False, type=int, default=5, help="Number classes to display.")
+    
+     parser.add_argument("-c", "--category_names", required=False, type=str, default='cat_to_name.json', help="Mapping of categories with real names.")
+    
+     parser.add_argument("-g", "--gpu", required=False, default=False, action='store_true', help="Chose to use gpu or not.")
+        
+     return parser
+
+        
+def process_image(image_path):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    
+    # Process a PIL image for use in a PyTorch model
+    
+    image_compose = transforms.Compose([transforms.Resize(255),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.485, 0.456, 0.406],
+                                                          [0.229, 0.224, 0.225])])
+    
+    loaded_image = Image.open(image_path)
+    image = image_compose(loaded_image)  
+    
+    return image
+
+    
+
+
